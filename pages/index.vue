c0e112beac2407ddf9e68e55d4cb60462e2a1f46
giraud florent <florent giraud>
2022-12-27 21:34:32 -0500

    feat: try out lite youtube

--- pages/index.vue
+++ pages/index.vue
@@ -12,7 +12,11 @@
       </Card>
       <main class="max-w-[1200px] mx-auto pb-20 md:pb-0">
         <section id="me">
-            <!-- :modifiers="{blur: 10}" -->
+          <lite-youtube
+            videoid="I7NKWGIewgM"
+            playlabel="Kingdom Hearts a failli avoir un anime : 26 anecdotes et secrets sur KH 1 🔥 JV Facts"
+            params="controls=0&start=10&end=30&modestbranding=2&rel=0&enablejsapi=1"
+          />
         </section>
         <section id="news" class="mb-8">
           <h2 class="text-headline mb-4">News about me</h2>
